c154ee789f2f76c1a114819c5e7c06cae26d075e
Tomasz <sobeczki@gmail.com>
2022-03-15 14:44:37 +0000

    Initial Commit. Created hello world application for DevOpsPipelines

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class App {
+
+  public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null,"Hello World","DevOpsTest",JOptionPane.INFORMATION_MESSAGE);
+
+}
+}
+
